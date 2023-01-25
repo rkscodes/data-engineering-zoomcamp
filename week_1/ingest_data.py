0b29594914c9ecf53f56a1f00fb74d1f3b8b08ad
Ram Krishna Singh <rkscodes@gmail.com>
2023-01-25 16:58:46 +0000

    copied gcp code to this branch

--- week_1/ingest_data.py
+++ week_1/ingest_data.py
@@ -8,14 +8,12 @@ from sqlalchemy import create_engine
 
 import pathlib
 
-import re
-
 
 def main(params):
     user = params.user
     password = params.password
-    host = params.host
-    port = params.port
+    host = params.host 
+    port = params.port 
     db = params.db
     table_name = params.table_name
     url = params.url
@@ -29,36 +27,36 @@ def main(params):
 
     df = pd.read_csv(f"{csv_name}")
 
-    fragement = re.search("csv.gz$", csv_name)
-
-    if fragement != None:
+    if(csv_name=="green_tripdata_2019-01.csv.gz"):
         df_itr = pd.read_csv(f"{csv_name}", iterator=True, chunksize=10000)
+
 
         df = next(df_itr)
 
-        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
-        df.head(0).to_sql(name=f"{table_name}", con=engine, if_exists="replace")
-        df.to_sql(name=f"{table_name}", con=engine, if_exists="append")
+
+        df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+        df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+
+
+        df.head(0).to_sql(name=f"{table_name}", con=engine, if_exists='replace')
+        df.to_sql(name=f"{table_name}", con=engine, if_exists='append')
 
         while True:
             begin_time = time()
             df = next(df_itr)
-            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-            df.to_sql(name=f"{table_name}", con=engine, if_exists="append")
+            df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+            df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+            df.to_sql(name=f"{table_name}", con=engine, if_exists='append')
             end_time = time()
             print(f"inserted 10,000 data in {end_time - begin_time}")
-    df.to_sql(name=f"{table_name}", con=engine, if_exists="replace")
+    df.to_sql(name=f"{table_name}", con=engine, if_exists='replace')
     # in the case of above stopiteration error this will not be exectued, so try to handle it
     os.system(f"rm {csv_name}")
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        "Description: Arguments parsers for data ingesting"
-    )
+if __name__ == '__main__' :
+    parser = argparse.ArgumentParser("Description: Arguments parsers for data ingesting")
 
     # user
     # password
@@ -66,19 +64,15 @@ if __name__ == "__main__":
     # port
     # db_name
     # table_name
-    # url
+    # url 
 
-    parser.add_argument("--user", required=True, help="username for postgres")
-    parser.add_argument("--password", required=True, help="password for postgres")
-    parser.add_argument(
-        "--host", required=True, help="address where postgress is hosted"
-    )
-    parser.add_argument(
-        "--port", required=True, help="port at which postgress is running"
-    )
-    parser.add_argument("--db", required=True, help="database name")
-    parser.add_argument("--table_name", required=True, help="table name")
-    parser.add_argument("--url", required=True, help="data download link")
+    parser.add_argument("--user",required=True, help="username for postgres")
+    parser.add_argument("--password",required=True, help="password for postgres")
+    parser.add_argument("--host",required=True, help="address where postgress is hosted")
+    parser.add_argument("--port",required=True, help="port at which postgress is running")
+    parser.add_argument("--db",required=True, help="database name")
+    parser.add_argument("--table_name",required=True, help="table name")
+    parser.add_argument("--url",required=True, help="data download link")
 
     args = parser.parse_args()
     main(args)
--- week_1/pipeline.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import sys
-
-print(sys.argv)
-
-day = sys.argv[1]
-
-print(f'job finshed successfully for{day}')
