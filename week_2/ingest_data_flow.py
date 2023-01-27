c8613cbc7bba882bcc96ed26f2450daf3e57c307
Ram Krishna Singh <rkscodes@gmail.com>
2023-01-27 23:32:25 +0530

    added sqlalchemy prefect block to handle database

--- week_2/ingest_data_flow.py
+++ week_2/ingest_data_flow.py
@@ -11,6 +11,7 @@ from sqlalchemy import create_engine
 from prefect import flow, task
 
 from prefect.blocks.system import String
+from prefect_sqlalchemy import SqlAlchemyConnector
 
 
 @task(log_prints=True, retries=3, cache_result_in_memory=True)
@@ -31,7 +32,7 @@ def extract_data(params):
 
 
 @task(log_prints=True)
-def transform_data(params, df):
+def transform_data(df):
     print(
         f"pre: missing passenger count: {df['passenger_count'].isin([0]).sum()}")
     df = df[df['passenger_count'] != 0]
@@ -42,16 +43,9 @@ def transform_data(params, df):
 
 @task()
 def load_data(params, df):
-    user = params.user
-    password = params.password
-    host = params.host
-    port = params.port
-    db = params.db
     table_name = params.table_name
-    url = params.url
-
-    engine = create_engine(
-        f'postgresql://{user}:{password}@{host}:{port}/{db}')
+    engine = SqlAlchemyConnector.load(
+        "local-postgres-docker-block").get_engine()
 
     df.to_sql(name=table_name, con=engine, if_exists='append')
 
@@ -67,20 +61,13 @@ def main_flow(params):
 
     log_subflow(args)
     data_raw = extract_data(args)
-    transformed_data = transform_data(args, data_raw)
+    transformed_data = transform_data(data_raw)
     load_data(args, transformed_data)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
 
-    parser.add_argument('--user', required=True, help='user name for postgres')
-    parser.add_argument('--password', required=True,
-                        help='password for postgres')
-    parser.add_argument('--host', required=True, help='host for postgres')
-    parser.add_argument('--port', required=True, help='port for postgres')
-    parser.add_argument('--db', required=True,
-                        help='database name for postgres')
     parser.add_argument('--table_name', required=True,
                         help='name of the table where we will write the results to')
     parser.add_argument('--url', required=True, help='url of the csv file')
@@ -105,11 +92,6 @@ if __name__ == '__main__':
 
 
 # [
-#     '--user=root',
-#     '--password=root',
-#     '--host=localhost',
-#     '--port=5432',
-#     '--db=ny_taxi',
 #     '--table_name=green_taxi_data',
 #     '--url=http://localhost:8000/output%20copy.csv.gz'
 # ]
