175aa490120a2a484fd1052b66b7c58d55f57607
Ram Krishna Singh <rkscodes@gmail.com>
2023-01-30 20:53:37 +0530

    week2 part2

---
+++ week_2/02_gcp/etl_web_to_gc.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+import pandas as pd
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+
+
+@task(retries=3)
+def fetch(dataset_url: str) -> pd.DataFrame:
+    """Read data from web into pandas dataframe"""
+    df = pd.read_csv(dataset_url)
+    return df
+
+
+@task(log_prints=True)
+def clean(df=pd.DataFrame) -> pd.DataFrame:
+    """Fix dtypes issues"""
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    print(df.head(2))
+    print(f"columns: {df.dtypes}")
+    print(f"no of rows: {len(df)}")
+    return df
+
+
+@task()
+def write_local(df: pd.DataFrame, colors: str, dataset_file: str) -> Path:
+    """Write Dataframe out as a parquet file"""
+    path = Path(f"data/{colors}/{dataset_file}.parquet")
+    df.to_parquet(path, compression="gzip")
+    return path
+
+
+@task()
+def write_gcs(path: Path) -> None:
+    """Write data to GCS DataLake"""
+    gcs_block = GcsBucket.load("taxi-gcs-data")
+    gcs_block.upload_from_path(from_path=path, to_path=path)
+    return
+
+
+@flow()
+def etl_web_to_gcs() -> None:
+    """ The main ETL function"""
+    color = "yellow"
+    year = 2021
+    month = 1
+    dataset_file = f"{color}_tripdata_{year}-{month:02}"
+    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
+
+    # dataset_url = "http://localhost:8000/yellow_tripdata_2021-01.csv.gz"
+    df = fetch(dataset_url)
+    df_clean = clean(df)
+    path = write_local(df_clean, color, dataset_file)
+    write_gcs(path)
+
+
+if __name__ == "__main__":
+    etl_web_to_gcs()
