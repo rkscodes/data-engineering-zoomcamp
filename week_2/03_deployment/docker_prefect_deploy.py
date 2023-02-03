6425dec1cc987c10cb5e308705769f76cc091062
Ram Krishna Singh <rkscodes@gmail.com>
2023-02-03 19:08:11 +0000

    added python script docker-prefect deploy

---
+++ week_2/03_deployment/docker_prefect_deploy.py
@@ -0,0 +1,12 @@
+from prefect.infrastructure.docker import DockerContainer
+from prefect.deployments import Deployment
+from parametrized_flow.py import etl_parent_flow
+
+docker_container_block = DockerContainer.load("docker-prefect")
+
+docker_dep = Deployment.build_from_flow(
+    flow = etl_parent_flow , name = "docker-flow" , infrastructure=docker_container_block
+)
+
+if __name__ = "__main__":
+    docker_dep.apply()
