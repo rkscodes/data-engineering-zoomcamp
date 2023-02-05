2b79a9c1b24936841764563d388fccf088610550
Ram Krishna Singh <rkscodes@gmail.com>
2023-02-05 11:34:26 +0000

    Question 1. Load January 2020 data Solution

--- week_2/02_gcp/etl_web_to_gc.py
+++ week_2/02_gcp/etl_web_to_gc.py
@@ -4,10 +4,11 @@ from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
 
 
-@task(retries=3)
+@task(retries=3, log_prints=True)
 def fetch(dataset_url: str) -> pd.DataFrame:
     """Read data from web into pandas dataframe"""
     df = pd.read_csv(dataset_url)
+    print(f"no of rows: {len(df)}")
     return df
 
 
@@ -22,15 +23,17 @@ def clean(df=pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-@task()
+@task(log_prints=True)
 def write_local(df: pd.DataFrame, colors: str, dataset_file: str) -> Path:
     """Write Dataframe out as a parquet file"""
     path = Path(f"data/{colors}/{dataset_file}.parquet")
+    directory = Path(f"data/{colors}/")
+    directory.mkdir(parents=True, exist_ok=True)     
     df.to_parquet(path, compression="gzip")
     return path
 
 
-@task()
+@task(log_prints=True)
 def write_gcs(path: Path) -> None:
     """Write data to GCS DataLake"""
     gcs_block = GcsBucket.load("taxi-gcs-data")
@@ -38,19 +41,19 @@ def write_gcs(path: Path) -> None:
     return
 
 
-@flow()
+@flow(log_prints=True)
 def etl_web_to_gcs() -> None:
     """ The main ETL function"""
-    color = "yellow"
-    year = 2021
+    color = "green"
+    year = 2020
     month = 1
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
 
     # dataset_url = "http://localhost:8000/yellow_tripdata_2021-01.csv.gz"
     df = fetch(dataset_url)
-    df_clean = clean(df)
-    path = write_local(df_clean, color, dataset_file)
+    # df_clean = clean(df)
+    path = write_local(df, color, dataset_file)
     write_gcs(path)
 
 
