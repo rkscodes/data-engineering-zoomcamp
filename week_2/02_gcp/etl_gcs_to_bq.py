c5197b6702b6b6e3c7c5101bdcd25b87c2374b9b
Ram Krishna Singh <rkscodes@gmail.com>
2023-02-05 15:57:00 +0000

    Question 3. Loading data to BigQuery Solved

--- week_2/02_gcp/etl_gcs_to_bq.py
+++ week_2/02_gcp/etl_gcs_to_bq.py
@@ -10,20 +10,24 @@ def extract_from_gcs(color: str, year: int, month: int) -> Path:
     "Download Trip data from GCS"
     gcs_path = f"data/{color}/{color}_tripdata_{year}-{month:02}.parquet"
     gcs_block = GcsBucket.load("taxi-gcs-data")
+
+    # make the required directory if not already
+    # gcs_dir =  Path(f"data/{color}")
+    # gcs_dir.mkdir(parents=True, exist_ok=True)
+
     gcs_block.get_directory(from_path=gcs_path, local_path=gcs_path)
     return Path(f"{gcs_path}")
 
 
 @task(log_prints=True)
 def transform(path: Path) -> pd.DataFrame:
-    "Data cleaning example"
+    # "fixing dataframe date time scenario "
     df = pd.read_parquet(path)
-    print(
-        f"pre missing passenger count : {df['passenger_count'].isna().sum()}")
-    df["passenger_count"].fillna(0, inplace=True)
-    print(
-        f"post missing passenger count : {df['passenger_count'].isna().sum()}")
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    print(df.dtypes)
     return df
+
 
 
 @task(retries=3)
@@ -34,18 +38,24 @@ def write_bq(color: str, df: pd.DataFrame) -> None:
         credentials=GcpCredentials.load("taxi-gcp-cred").get_credentials_from_service_account())
 
 
-@flow()
-def etl_gcs_to_bg():
-    color = "yellow"
-    year = 2021
-    month = 1
+@flow(log_prints=True)
+def etl_gcs_to_bg(color: str, year: int, month: int) -> int:
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
-    # dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
-    # path = extract_from_gcs(color, year, month)
-    path = f"data/yellow/{dataset_file}.parquet"
-    clean_df = transform(path)
-    write_bq(color, clean_df)
+    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
+    path = extract_from_gcs(color, year, month)
 
+    clean_df = transform(path)
+    # print(path)
+    print(f"no of rows: {len(clean_df)}")
+    write_bq(color, clean_df)
+    return len(clean_df)
+
+@flow(log_prints=True)
+def etl_gcs_to_bg_parent(color:str = "yellow", year: int = 2019, months : list = [2,3]) -> None:
+    ans = 0 
+    for month in months:
+        ans += etl_gcs_to_bg(color, year ,month)
+    print(f"Total no of inserted values: {ans}")
 
 if __name__ == "__main__":
-    etl_gcs_to_bg()
+    etl_gcs_to_bg_parent()
