e7490067a22a0d70e69bc875a9761e58b79bc6b9
Ram Krishna Singh <rkscodes@gmail.com>
2023-02-05 12:10:39 +0000

    Question 2. Scheduling with Cron Solved

--- week_2/03_deployment/parametrized_flow.py
+++ week_2/03_deployment/parametrized_flow.py
@@ -6,18 +6,20 @@ from prefect.tasks import task_input_hash
 from datetime import timedelta
 
 
-@task(retries=3, cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
+@task(retries=3, cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1), log_prints=True)
 def fetch(dataset_url: str) -> pd.DataFrame:
     """Read data from web into pandas dataframe"""
     df = pd.read_csv(dataset_url)
+    print(df.columns)
+    print(f"no of rows: {len(df)}")
     return df
 
 
 @task(log_prints=True)
 def clean(df=pd.DataFrame) -> pd.DataFrame:
     """Fix dtypes issues"""
-    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+    df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
     print(df.head(2))
     print(f"columns: {df.dtypes}")
     print(f"no of rows: {len(df)}")
@@ -28,6 +30,8 @@ def clean(df=pd.DataFrame) -> pd.DataFrame:
 def write_local(df: pd.DataFrame, colors: str, dataset_file: str) -> Path:
     """Write Dataframe out as a parquet file"""
     path = Path(f"data/{colors}/{dataset_file}.parquet")
+    directory = Path(f"data/{colors}/")
+    directory.mkdir(parents=True, exist_ok=True) 
     df.to_parquet(path, compression="gzip")
     return path
 
@@ -40,7 +44,7 @@ def write_gcs(path: Path) -> None:
     return
 
 
-@flow()
+@flow(log_prints=True)
 def etl_web_to_gcs(color: str, year: int, month: int) -> None:
     """ The main ETL function"""
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
@@ -48,19 +52,19 @@ def etl_web_to_gcs(color: str, year: int, month: int) -> None:
 
     # dataset_url = "http://localhost:8000/yellow_tripdata_2021-01.csv.gz"
     df = fetch(dataset_url)
-    df_clean = clean(df)
-    path = write_local(df_clean, color, dataset_file)
+    # df_clean = clean(df)
+    path = write_local(df, color, dataset_file)
     write_gcs(path)
 
 
-@flow()
+@flow(log_prints=True)
 def etl_parent_flow(color: str, year: int, months: list[int] = [1, 2]):
     for month in months:
         etl_web_to_gcs(color, year, month)
 
 
 if __name__ == "__main__":
-    color = "yellow"
-    months = [1, 2, 3]
-    year = 2021
+    color = "green"
+    months = [1]
+    year = 2020
     etl_parent_flow(color, year, months)
