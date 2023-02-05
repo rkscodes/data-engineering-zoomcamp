42070fc4a5cb705a8c667376e7fe42931692e1eb
Ram Krishna Singh <rkscodes@gmail.com>
2023-02-05 17:59:52 +0000

    Question 4. Github Storage Block Solved

--- week_2/02_gcp/github_deployment.py
+++ week_2/02_gcp/github_deployment.py
@@ -5,7 +5,7 @@ from etl_web_to_gc import etl_web_to_gcs
 github_block = GitHub.load("homework-github")
 
 github_dep = Deployment.build_from_flow(
-    flow=etl_web_to_gcs, name="github-flow", storage=github_block, entrypoint="week_2/02_gcp/etl_web_to_gc.py"
+    flow=etl_web_to_gcs, name="github-flow", storage=github_block, entrypoint="week_2/02_gcp/etl_web_to_gc.py:etl_web_to_gcs"
 )
 
 if __name__ == "__main__":
