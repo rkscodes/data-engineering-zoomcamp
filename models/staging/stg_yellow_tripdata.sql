70a41abd3e58c58d9e2a8ff229d972565acd3c38
Ram Krishna Singh <30290728+rkscodes@users.noreply.github.com>
2023-02-21 12:10:24 +0530

    Changed default number of rows from 100 to all

--- models/staging/stg_yellow_tripdata.sql
+++ models/staging/stg_yellow_tripdata.sql
@@ -43,4 +43,4 @@ select
 from tripdata
 where rn =1
 -- dbt build --m <model.sql> --var 'is_test_run: false'
-{% if var("is_test_run", default=true) %} limit 100 {% endif %}
+{% if var("is_test_run", default=false) %} limit 100 {% endif %}
