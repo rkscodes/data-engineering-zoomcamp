ea6cc5969c629f0a8e1d359749c744d7f4bc4cb8
Ram Krishna Singh <30290728+rkscodes@users.noreply.github.com>
2023-02-21 11:30:09 +0530

    Update stg_yellow_tripdata.sql
    changed lpep to tpep

--- models/staging/stg_yellow_tripdata.sql
+++ models/staging/stg_yellow_tripdata.sql
@@ -3,14 +3,14 @@
 with tripdata as 
 (
   select *,
-    row_number() over(partition by cast(vendorid as integer), lpep_pickup_datetime) as rn
+    row_number() over(partition by cast(vendorid as integer), tpep_pickup_datetime) as rn
   from {{ source('staging','yellow_tripdata') }}
   where vendorid is not null 
 )
 
 select
     -- identifiers
-    {{ dbt_utils.surrogate_key(["vendorid", "lpep_pickup_datetime"]) }} as tripid,
+    {{ dbt_utils.surrogate_key(["vendorid", "tpep_pickup_datetime"]) }} as tripid,
     cast(vendorid as integer) as vendorid,
     cast(ratecodeid as integer) as ratecodeid,
     cast(pulocationid as integer) as pickup_locationid,
